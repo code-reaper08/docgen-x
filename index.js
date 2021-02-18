003324c250942de66a12cc2cf005788b5ae913f1
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-02-18 16:38:24 +0530

    Added all the needed files

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const fs = require("fs");
 
-exports.writeHTML5pub = writeHTML5;
-function writeHTML5(){
-    const html5 = fs.readFileSync(`${__dirname}/temp/LICENSEFILE.md`)
+exports.writemitpub = writemit;
+exports.writeapapub = writeapa;
+exports.writegnu3pub = writegnu3;
+exports.writegnu2pub = writegnu2;
+exports.writebsd2pub = writebsd2;
+function writemit(){
+    const mit = fs.readFileSync(`${__dirname}/temp/MIT.md`)
     const filename = process.argv[2]
-    fs.writeFileSync(`${process.cwd()}/${filename}`, html5)
+    fs.writeFileSync(`${process.cwd()}/${filename}`, mit)
 }
 
-writeHTML5()
+function writeapa(){
+    const apa = fs.readFileSync(`${__dirname}/temp/APA.md`)
+    const filename = process.argv[2]
+    fs.writeFileSync(`${process.cwd()}/${filename}`, apa)
+}
+
+function writegnu3(){
+    const gnu3 = fs.readFileSync(`${__dirname}/temp/GNU3.md`)
+    const filename = process.argv[2]
+    fs.writeFileSync(`${process.cwd()}/${filename}`, gnu3)
+}
+
+function writegnu2(){
+    const gnu2 = fs.readFileSync(`${__dirname}/temp/GNU2.md`)
+    const filename = process.argv[2]
+    fs.writeFileSync(`${process.cwd()}/${filename}`, gnu2)
+}
+
+function writebsd2(){
+    const bsd2 = fs.readFileSync(`${__dirname}/temp/BSD2.md`)
+    const filename = process.argv[2]
+    fs.writeFileSync(`${process.cwd()}/${filename}`, bsd2)
+}
+
+writemit()
+writeapa()
+writegnu3()
+writegnu2()
+writebsd2()
